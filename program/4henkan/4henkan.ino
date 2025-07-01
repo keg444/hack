a6dd96679fc0f04950f0416d95d66034b4baa5dd
keg444 <kugajo_0605@icloud.com>
2025-07-02 08:11:40 +0900

    finish final report

--- program/4henkan/4henkan.ino
+++ program/4henkan/4henkan.ino
@@ -1,35 +1,35 @@
 const int maxmoji = 256 * 4;  // 最大文字数256文字分（ASCII）×4桁
 int base4[maxmoji];           // 4進数の結果を格納する配列
 int resultIndex = 0;
+int time = 5;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');  //改行まで読み込む
+    String input = Serial.readStringUntil('\n');
     resultIndex = 0;
 
-    for (int i = 0; i < input.length(); i++) {  //読み込んだ文字列の長さを取得
-      char c = input.charAt(i); //１文字ずつcに代入
-      byte ascii = (byte)c; //10進数のasciiコードに変換
-      getBase4(ascii);  //4進数に変換
+    for (int i = 0; i < input.length(); i++) {
+
+      char c = input.charAt(i);
+      byte ascii = (byte)c;
+      getBase4(ascii);
     }
+
+    Base4sousin();
     
-    //確認用
-    String s="";
-    int k;
-    Serial.print("Base4: ");
     for (int i = 0; i < resultIndex; i++) {
       Serial.print(base4[i]);
-      s += String(base4[i]);
     }
     Serial.println("");
-    for(k=0; k<resultIndex; k+=2){
-      Serial.println(s.substring(k, k+2));
-    }
-    Serial.println("-------------------------");
   }
 }
 
@@ -40,6 +40,29 @@ void getBase4(byte b) {
     b = b / 4;
   }
   for (int i = 0; i < 4; i++) {
-    base4[resultIndex++] = amari[i];  //resultIndexに代入して1増やす
+    base4[resultIndex++] = amari[i];
   }
+}
+
+void Base4sousin() {
+  digitalWrite(A4, HIGH);
+
+  for (int k = 0; k < resultIndex; k++) {
+    int pin;
+    if (base4[k] == 0) {
+      pin = A0;
+    } else if (base4[k] == 1) {
+      pin = A1;
+    } else if (base4[k] == 2) {
+      pin = A2;
+    } else {
+      pin = A3;
+    }
+
+    digitalWrite(pin, HIGH);
+    delay(time);              // この時間内に受信側が読み取り
+    digitalWrite(pin, LOW);
+  }
+
+  digitalWrite(A4, LOW);    // 送信終了通知
 }
